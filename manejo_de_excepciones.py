37034252b2f51f3e8741e2bcafa1335bb397c9a8
William <freddymartinez896@gmail.com>
2021-06-05 01:37:33 -0700

    Segundo commit: Agrego ejercicio de debugging y Manejo de excepciones(try,except, raise).

---
+++ manejo_de_excepciones.py
@@ -0,0 +1,20 @@
+def divisors(num):
+
+    divisors = [i for i in range(1, num + 1) if num % i == 0]
+    return divisors
+    
+def run():
+    
+    try:
+        num = int(input("Ingresa un numero: "))
+        if num <= 0:
+            raise Exception("Numero negativo o diferente a cero")
+        print(divisors(num))
+        print("END PROGRAM")
+    except ValueError:
+        print("Ingresa un numero entero.")
+    except Exception:
+        print("Debes Ingresar Un Numero Positivo")
+
+if __name__ == '__main__':
+    run()
