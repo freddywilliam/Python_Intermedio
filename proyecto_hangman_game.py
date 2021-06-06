0960430ab44ae3c4b4acc0d79f832910d1d4008e
William <freddymartinez896@gmail.com>
2021-06-06 03:06:31 -0700

    En este commit agrego el tema de las rayas, cuando el usuartio ingresa la letra, la misma aparece en la posicion exacta dentro de la lista de guiones.

--- proyecto_hangman_game.py
+++ proyecto_hangman_game.py
@@ -22,7 +22,6 @@ def importar_palabra():
     del palabra_cortada_enumerada[contador]
     dic_palabra = palabra_cortada_enumerada
     dic_palabra =list(dic_palabra.values())
-    print(dic_palabra)
 
     return dic_palabra
 
@@ -34,62 +33,90 @@ def palabra_usuario():
 
     return dic_letras_ingrsadas_numeradas
 
-def rayas(palabra):
-    numero_rayas = len(palabra)
-    print(" - " * numero_rayas)
 
 def comparacion_listas_cierre(a, b):
     return Counter(a) == Counter(b)
 
-
     
-def comparacion(letra_usuario, palabra_importada, letra_adivinada):
+def comparacion(letra_usuario, palabra_importada, letra_adivinada, palabra_rayada):
+    
+
+    while len(palabra_rayada) < len(palabra_importada):
+        for i in range(len(palabra_importada)):
+
+            palabra_rayada.append(' _ ')
+        
+       
 
     for i in range(len(palabra_importada)):
-       
-        if palabra_importada[i]== letra_usuario[0]:
-            letra_adivinada.append(palabra_importada[i])
-        
-    return letra_adivinada
 
-def letras_repetidas(lista_letra_usuario):
-    mensaje = 0
+        
+        if palabra_importada[i]== letra_usuario[0]:
+            os.system("cls")
+            letra_adivinada.append(palabra_importada[i])
+            palabra_rayada[i]= letra_usuario
+            print(palabra_rayada)
+        
+        # else:
+        #     os.system("cls")
+
+    
+        
+    return letra_adivinada, palabra_rayada
+
+def letras_repetidas(lista_letra_usuario, mensaje):
+    
     letras_repetidas = []
+    mensaje = 0
 
     for n in lista_letra_usuario:
         if n not in letras_repetidas:
             letras_repetidas.append(n)
+            mensaje = 0
+            
         else:
             mensaje = 1
+            
+            
     return mensaje
-
 
 def run():
     os.system("cls")
     palabra_importada = importar_palabra()
-    dibujo_rayas = rayas(palabra_importada)
+
+    # letra_usuario_rayas = palabra_usuario()
+    # rayas(palabra_importada, letra_usuario_rayas)
+    palabra_rayada = []
 
     lista_letra_usuario = []
 
     letra_adivinada = []
     comparacion_listas_cierre_valor = 0
 
+    mensaje = 0
+
+  
     for i in range(10):
-       
-        # print(comparacion_listas_cierre_valor)
+        
+        print(" ")
         letra_usuario = palabra_usuario()
 
-        
         lista_letra_usuario.append(letra_usuario)
-        letras_repetidas(lista_letra_usuario)
+        letras_repetidas(lista_letra_usuario, mensaje)
 
-        if letras_repetidas(lista_letra_usuario) == 1:
+        if letras_repetidas(lista_letra_usuario, mensaje) == 1:
             print("REPETIDA")
+            letras_repetidas(lista_letra_usuario, mensaje)
+            mensaje = 0 
+            
         
-        if letras_repetidas(lista_letra_usuario) == 0: 
-            comparacion(letra_usuario, palabra_importada, letra_adivinada)
+        if letras_repetidas(lista_letra_usuario, mensaje) == 0: 
+    
+            comparacion(letra_usuario, palabra_importada, letra_adivinada, palabra_rayada)
             comparacion_listas_cierre_valor = comparacion_listas_cierre(palabra_importada, letra_adivinada)
-            print(letra_adivinada)
+            # print(letra_adivinada)
+            mensaje = 0
+
             if comparacion_listas_cierre_valor == True:
                 print("GANASTE")
                 break
